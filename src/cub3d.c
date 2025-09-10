845ba9cc4c030f95a88ef875412e523a6890cff6
WSL HOME <raulrecrec@gmail.com>
2025-09-10 20:02:14 +0200

    Update main to check for correct argument usage
    - Added argument count validation in main.
    - Prints usage message with program name if the number of arguments is not 2.
    - Ensures the program exits cleanly with return code 1 on incorrect usage.

--- src/cub3d.c
+++ src/cub3d.c
@@ -3,20 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 18:09:27 by daniema3          #+#    #+#             */
-/*   Updated: 2025/08/17 19:27:15 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/09/10 20:01:13 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-int	main(void)
+int	main(int argc, char *argv)
 {
 	void	*memory_leak;
 
 	memory_leak = malloc(42);
 	(void) memory_leak;
+	if (argc != 2)
+	{
+		ft_printf("Uso: %s <mapa.ber>\n", argv[0]);
+		return (1);
+	}
 	return (0);
 }
