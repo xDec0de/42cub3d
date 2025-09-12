d7ee56f1a4a92681bb960f41bd02185fa8504c98
WSL HOME <raulrecrec@gmail.com>
2025-09-12 12:54:15 +0200

    Update parser.h to include cb_types.h and cb_msg.h
    - Replaced the old cub3d.h include with cb_types.h and cb_msg.h.
    - Improved code organization and readability by separating types and messages.
    - Ensured parser.h now references the updated header structure.

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 19:12:23 by rexposit          #+#    #+#             */
-/*   Updated: 2025/09/10 19:37:30 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/09/12 12:49:44 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdbool.h>
 # include <stddef.h>
-# include "cub3d.h"
+# include "./cb_types.h"
+# include "./cb_msg.h"
 
 #endif
