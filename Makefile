5e4f3857349be60e76910cb772452ac72783d4c0
xDec0de <xdec0deyt@gmail.com>
2025-09-13 17:27:02 +0200

    Added cb_split

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/08/08 18:07:28 by daniema3          #+#    #+#              #
-#    Updated: 2025/09/13 09:29:16 by daniema3         ###   ########.fr        #
+#    Updated: 2025/09/13 17:03:38 by daniema3         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -46,7 +46,8 @@ SRCS +=	util/mem/cb_malloc.c
 
 # > ~ Utils - Strings
 
-SRCS +=	util/str/cb_strendswith.c \
+SRCS +=	util/str/cb_split.c \
+		util/str/cb_strendswith.c \
 		util/str/cb_strlen.c
 
 # > ~ Auto free & double free prevention (I'M NOT SURE IF WE CAN DO THIS)
--- src/util/str/cb_strutils.h
+++ src/util/str/cb_strutils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cb_strutils.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 04:37:37 by daniema3          #+#    #+#             */
-/*   Updated: 2025/09/12 06:19:29 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/09/13 17:02:57 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "cb_types.h"
 
+char	**cb_split(const char *s, char c);
 size_t	cb_strlen(char *str);
 bool	cb_strendswith(char *str, char *suffix);
 
