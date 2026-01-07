e33e9c961d2820b6e4230b427a6f866f80e20b5d
WSL HOME <raulrecrec@gmail.com>
2026-01-07 09:30:43 +0100

    Add new file render.h and implement basic render entrypoint
    - Created render.h to define the render module interface and required includes.
    - Declared fake_cb_get() to fetch hardcoded game data during early render development.
    - Created render.c with an initial render() stub that retrieves the game instance for testing.

---
+++ src/render/render.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/07 09:10:05 by rexposit          #+#    #+#             */
+/*   Updated: 2026/01/07 09:13:07 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RENDER_H
+# define RENDER_H
+
+# include <stdbool.h>
+# include <stddef.h>
+# include "./cb_types.h"
+# include "./cb_msg.h"
+
+t_game	*fake_cb_get(void);
+
+#endif
