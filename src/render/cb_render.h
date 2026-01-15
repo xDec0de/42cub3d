cf1cfd3204b98986e021e991866ba8a60a86bf76
WSL HOME <raulrecrec@gmail.com>
2026-01-15 20:03:42 +0100

    Update render.h to include MiniLibX, window constants, and fake_cb_exit
    - Included the MiniLibX header in render.h to expose mlx types and functions to the render module.
    - Defined WINDOW_WIDTH, WINDOW_HEIGHT, and TILE_SIZE constants for window creation and render scaling.
    - Added the fake_cb_exit prototype so the render side can exit cleanly using the fake hardcoded workflow.

--- src/render/cb_render.h
+++ src/render/cb_render.h
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 09:10:05 by rexposit          #+#    #+#             */
-/*   Updated: 2026/01/07 13:10:09 by rexposit         ###   ########.fr       */
+/*   Updated: 2026/01/15 19:53:29 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,14 @@
 # include <stddef.h>
 # include "./cb_types.h"
 # include "./cb_msg.h"
+# include <mlx.h>
+
+# define WINDOW_WIDTH 800
+# define WINDOW_HEIGHT 600
+# define TILE_SIZE 64
 
 void	render(void);
 t_game	*fake_cb_get(void);
+void	fake_cb_exit(char *msg, int ec);
 
 #endif
