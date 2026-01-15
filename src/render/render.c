21c01da8a61ba309683b00681f3afd23e3f16e51
WSL HOME <raulrecrec@gmail.com>
2026-01-15 22:16:01 +0100

    norm correction

--- src/render/render.c
+++ src/render/render.c
@@ -6,12 +6,11 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 09:07:46 by rexposit          #+#    #+#             */
-/*   Updated: 2026/01/15 21:59:36 by rexposit         ###   ########.fr       */
+/*   Updated: 2026/01/15 22:15:12 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cb_render.h"
-#include <stdio.h>
 
 void	render(void)
 {
@@ -21,7 +20,8 @@ void	render(void)
 	game->mlx = mlx_init();
 	if (!game->mlx)
 		fake_cb_exit(NULL, -3);
-	game->window = mlx_new_window(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "cub3d");
+	game->window = mlx_new_window(game->mlx, WINDOW_WIDTH,
+			WINDOW_HEIGHT, "cub3d");
 	if (!game->window)
 		fake_cb_exit(NULL, -3);
 	mlx_loop(game->mlx);
