467e816ca040bd49398d90bfae0a966b706b0dbb
xdec0de <xdec0deyt@gmail.com>
2026-02-14 18:04:36 +0100

    Fix known memory leaks with normal program execution
    Some leaks may still be present in the parser itself when invalid input is provided (Early program termination). I'll try to implement CST to test the parser instead of searching for leaks manually

--- src/cb_exit.c
+++ src/cb_exit.c
@@ -3,19 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   cb_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 04:34:46 by daniema3          #+#    #+#             */
-/*   Updated: 2025/09/13 12:46:07 by daniema3         ###   ########.fr       */
+/*   Updated: 2026/02/14 17:59:04 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
+#include "mlx.h"
 #include "cb_strutils.h"
+#include "cb_mem.h"
 
 void	free_game(t_game *game)
 {
+	if (game->map.grid != NULL)
+		cb_arrfree((void **) game->map.grid);
+	if (game->mlx && game->window)
+		mlx_destroy_window(game->mlx, game->window);
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+		game->mlx = NULL;
+	}
 	free(game);
 }
 
